7fa9aed83413092432a2afd95a218a20e2f56b73
Colton Lathrop <colathro@microsoft.com>
2022-09-24 10:55:36 -0700

    mysql

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
 #[macro_use]
 extern crate rocket;
 
+use std::error::Error;
+
 use rocket::{http::Status, response::status};
-use rocket_db_pools::{mongodb, Connection, Database};
+use rocket_db_pools::{
+    sqlx::{self, Row},
+    Connection, Database,
+};
 
 #[derive(Database)]
 #[database("mob")]
-struct MongoDBClient(mongodb::Client);
+struct DBClient(sqlx::MySqlPool);
 
 #[launch]
 async fn rocket() -> _ {
     println!("Mob has started.");
 
     rocket::build()
-        .attach(MongoDBClient::init())
+        .attach(DBClient::init())
         .mount("/api/test", routes![hello_world])
 }
 
 #[get("/helloworld")]
-async fn hello_world(client: Connection<MongoDBClient>) -> status::Custom<String> {
-    let opt_dbs = client.list_database_names(None, None).await;
-    match opt_dbs {
-        Ok(dbs) => {
-            for db_name in dbs {
-                println!("{}", db_name);
-            }
-            status::Custom(Status::Ok, String::from("Everything is OK!"))
+async fn hello_world(mut client: Connection<DBClient>) -> status::Custom<String> {
+    let query_resp: Option<String> = sqlx::query("SELECT * FROM users")
+        .fetch_one(&mut *client)
+        .await
+        .and_then(|r| Ok(r.try_get(2)?))
+        .ok();
+
+    match query_resp {
+        Some(resp) => {
+            return status::Custom(Status::Ok, String::from(resp));
         }
-        Err(err) => panic!("Problem opening the file: {:?}", err),
+        None => status::Custom(Status::Ok, String::from("Everything is BAD!")),
     }
 }
