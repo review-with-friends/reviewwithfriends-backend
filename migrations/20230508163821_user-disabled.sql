4796a172221815693b82ed4a321d594cc47ad11f
colton@spacedoglabs.com <colton@spacedoglabs.com>
2023-05-31 09:28:00 -0700

    remove used migrations

--- migrations/20230508163821_user-disabled.sql
+++
@@ -1,3 +0,0 @@
--- Add migration script here
-ALTER TABLE `mob`.`user`
-ADD COLUMN `disabled` BOOLEAN NOT NULL DEFAULT false AFTER `email`;
--- migrations/20230530033316_friendrequest-nulls.sql
+++
@@ -1,6 +0,0 @@
--- Add migration script here
-ALTER TABLE `mob`.`friendrequest`
-CHANGE COLUMN `created` `created` DATETIME NOT NULL ,
-CHANGE COLUMN `user_id` `user_id` VARCHAR(36) NOT NULL ,
-CHANGE COLUMN `friend_id` `friend_id` VARCHAR(36) NOT NULL ,
-CHANGE COLUMN `ignored` `ignored` TINYINT(1) NOT NULL ;
